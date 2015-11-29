8d37049a37c3261f9b8d75d056db206923c459ab
Lóna Lore <lonalore@freemail.hu>
2015-11-29 13:58:03 +0100

    Hungarian translations.

---
+++ languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Definition of hungarian strings for Admin UI.
+ */
+
+define('LAN_NPMA_01', 'Beállítások');
+define('LAN_NPMA_02', 'Értesítő üzenetek megjelenítése');
+define('LAN_NPMA_03', 'Üzenet hossza');
+define('LAN_NPMA_04', 'Értesítőben megjelenő üzenet maximális hossza.');
+define('LAN_NPMA_05', 'Hangjelzés bekapcsolás az értesítőkhöz');
+define('LAN_NPMA_06', '');
---
+++ languages/Hungarian/Hungarian_global.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Definition of hungarian strings for common usage.
+ */
+
+define('LAN_PLUGIN_NODEJS_PM_NAME', 'Node.js PM');
+define('LAN_PLUGIN_NODEJS_PM_SUMM', 'Node.js Privát üzenet');
+define('LAN_PLUGIN_NODEJS_PM_DESC', 'Valósidejű értesítők megjelenítése privát üzenet érkezésekor.');
+define('LAN_PLUGIN_NODEJS_PM_LINK', '');
